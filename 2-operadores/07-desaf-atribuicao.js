63dec38ac1dd83b4f60b59fa1ccde5f9f13cf00b
Negrão, IPR <neurocientista@gmail.com>
2024-06-05 13:15:55 -0300

    .

---
+++ 2-operadores/07-desaf-atribuicao.js
@@ -0,0 +1,11 @@
+//let texto = 'Bom dia!\nTudo bem?';
+//console.log(texto)
+
+
+let texto = 'Lista de Aprovados\n';
+texto +=  '==================\n';
+texto += '1. Ana Siva\n';
+texto += '2. Predo Moreno\n';
+texto += '3. Rebeca FRança\n';
+
+console.log(texto)
