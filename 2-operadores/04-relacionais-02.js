10c1817d3dd63a385da52ce8c95472dc2589c4b3
Negrão, IPR <neurocientista@gmail.com>
2024-06-05 11:15:41 -0300

    .

---
+++ 2-operadores/04-relacionais-02.js
@@ -0,0 +1,10 @@
+let a = 1
+let b = '1'
+let c = '1'
+
+console.log(a == b) //verdadeiro
+console.log(a === b) //falso
+console.log(b === c) //verdadeiro
+console.log(a != b) //falso
+console.log(a !== b) //verdadeiro
+
