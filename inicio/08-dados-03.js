f132376d21eb38a71d1e7ba4b9b4c63760145350
Negrão, IPR <neurocientista@gmail.com>
2024-06-04 23:59:24 -0300

    .

---
+++ inicio/08-dados-03.js
@@ -0,0 +1,14 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario)
+
+let estaChovendo = true
+let estaChovendo1 = false
+
+console.log(typeof estaChovendo)
+console.log(typeof estaChovendo1)
+
+console.log(typeof 'teste')
