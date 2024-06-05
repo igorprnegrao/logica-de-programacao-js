c81fe05059722fb625f804006a10b0115591a6e2
Negrão, IPR <neurocientista@gmail.com>
2024-06-05 11:34:30 -0300

    .

---
+++ 2-operadores/05-desaf-relacionais.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2;
+
+console.log(b > a);
+console.log(a + a == b);
+console.log(a < b);
+console.log(a !== b);
+console.log(a <= b)
+
+console.log('==========')
+
+console.log(a + a !== b);
+console.log(b < a);
+console.log(a >= b);
+console.log(a === b);
+console.log(a >= b)
