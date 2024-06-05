ba741d09968685138246af62139eddf9cb242809
Negrão, IPR <neurocientista@gmail.com>
2024-06-05 11:56:19 -0300

    .

---
+++ 2-operadores/06-atribuicao.js
@@ -0,0 +1,20 @@
+let x = 10;
+x = x + 6;
+x = x -1;
+
+let y = 20;
+y += 10;
+y -= 2;
+y *= 2;
+y /= 4;
+y %= 2;
+
+
+console.log(x);
+console.log(y);
+
+let w = 'texto';
+w += ', mais texto';
+w += '!!!';
+
+console.log(w)
