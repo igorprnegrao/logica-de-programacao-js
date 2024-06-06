d87f7dfaeb7e6adac452cd0b4409967b6e5ddc2b
Negrão, IPR <neurocientista@gmail.com>
2024-06-06 13:27:51 -0300

    .

---
+++ 2-operadores/10-desaf-logicos.js
@@ -0,0 +1,22 @@
+console.log('=======True======')
+let verdadeiro1 = 2 + 2 >= 5 && 3 != 2;
+console.log('verdadeiro1 = ', 2 + 2 >= 5,'&&' ,3 != 2);
+console.log('verdadeiro1 = ', verdadeiro1 )
+let verdadeiro2 = !2 === 2 || 10 >= 8;
+console.log('verdadeiro1 = ', !2 === 2,'ou' ,10 >= 8);
+console.log('verdadeiro2 = ', verdadeiro2 )
+
+console.log('=======False======')
+
+let falso1 = 5 != 5 || 5 <= 2;
+console.log('false1 = ', 5 != 5,'ou' ,5 <= 2);
+
+console.log('False1 = ', falso1 )
+
+let falso2 = 6 + 8 == 12 * 2 && 9 != 2;
+console.log('false2 = ', 6 + 8 == 12 * 2,'&&' ,9 != 2);
+console.log('False2 = ', falso2 )
+
+
+
+
