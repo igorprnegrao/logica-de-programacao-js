ce110f7e5f16ce776b4ce15ad20df06c1a5f850c
Negrão, IPR <neurocientista@gmail.com>
2024-06-09 22:17:19 -0300

    .

--- 2-operadores/15-ternario-02.js
+++ 2-operadores/15-ternario-02.js
@@ -1,4 +1,3 @@
 const hora = 23;
-const saudacao = 
-    hora <= 11 ? 'Bom dia' : hora <= 17 ? 'Boa tarde' : 'Boa noite';
+const saudacao = hora <= 11 ? 'Bom dia' : hora <= 17 ? 'Boa tarde' : 'Boa noite';
 console.log(saudacao)
