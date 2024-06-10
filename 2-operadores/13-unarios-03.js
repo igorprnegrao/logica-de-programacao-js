1a71dc074524519ba87d331b9daf938b769ea5a6
Negrão, IPR <neurocientista@gmail.com>
2024-06-09 21:52:59 -0300

    .

---
+++ 2-operadores/13-unarios-03.js
@@ -0,0 +1,10 @@
+console.log(!true);
+console.log(!false);
+
+console.log(!'texto');
+console.log(!!'');
+console.log(!!' ');
+console.log(!!1);
+console.log(!!0);
+console.log(!!-1);
+console.log(!!-0.00011);
