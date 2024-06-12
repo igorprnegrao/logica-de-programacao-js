065f8a91b5931abc60d5d22504b639024848a88f
Negrão, IPR <neurocientista@gmail.com>
2024-06-12 12:12:20 -0300

    .

--- 3-controle/04-desaf-IF-04.js
+++ 3-controle/04-desaf-IF-04.js
@@ -1,7 +1,9 @@
-const nota = 9;
+const nota = 2.3;
 if (nota >= 9) {
     console.log("Quadro de honra");
     console.log("Parabéns");
-} 
+} else {
+    console.log("Que pena :(");
+}
 
-console.log("FIM!")
+console.log("FIM!");
