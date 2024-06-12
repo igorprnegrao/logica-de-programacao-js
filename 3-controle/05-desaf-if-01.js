ecb0cf04107f076267e96fdec3b91497b67c939f
Negrão, IPR <neurocientista@gmail.com>
2024-06-12 13:33:38 -0300

    .

--- 3-controle/05-desaf-if-01.js
+++ 3-controle/05-desaf-if-01.js
@@ -1,18 +1,18 @@
-const nota = 7.99;
+const nota = 2.99;
 
 if (nota < 4.5) {
     console.log("Conceito: F ")
-} 
+};
 if (nota >= 4.5 && nota < 5) {
     console.log("Conceito: D ");
-} 
+};
 if (nota >= 5 && nota < 7) {
     console.log("Conceito: C ");
-} 
+}; 
 if (nota >= 7 && nota < 9) {
     console.log("Conceito: B ");
-} 
+};
 if (nota >= 9 && nota < 11){
     console.log("Conceito: A ") ;
-}
+};
     
