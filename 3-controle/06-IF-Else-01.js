f26e80a5c8cbe5c9141bb21c5ecec9540ad7564f
Negrão, IPR <neurocientista@gmail.com>
2024-06-12 13:48:58 -0300

    .

---
+++ 3-controle/06-IF-Else-01.js
@@ -0,0 +1,15 @@
+const verdade = true;
+const falsa = false;
+
+if(verdade)
+    console.log("Exec...")
+else
+    console.log("Não Exec...");
+
+if (falsa) {
+    console.log("Não Exec...");
+} else {
+    console.log("Exec...");
+}
+
+
