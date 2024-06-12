5d1284836de39fdcf1ab6a71c2523a63287c59db
Negrão, IPR <neurocientista@gmail.com>
2024-06-12 14:25:00 -0300

    .

--- 3-controle/09-IF-else-04.js
+++ 3-controle/09-IF-else-04.js
@@ -2,7 +2,7 @@ const hora = 21;
 let saudacao;
 
 if (hora < 12) {
-  saudacao = "Bom dia!";
+    saudacao = "Bom dia!";
 } else if (hora < 18) {
     saudacao = "Boa tarde";
 } else if (hora < 22) {
